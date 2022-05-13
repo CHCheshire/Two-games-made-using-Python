9ad869d78ad398355ae6eeb0b40e6d52115adf5e
CHCheshire <pringl31@gmail.com>
2022-05-13 15:30:28 +0000

    Add a start to the text adventure game

--- escape_from_the_manor.py
+++ escape_from_the_manor.py
@@ -1,5 +1,7 @@
 import os
 import random
+import subprocess
+import time
 
 
 def clear():
@@ -7,3 +9,20 @@ def clear():
     Clear function to clean-up the terminal so things don't get messy.
     """
     os.system("cls" if os.name == "nt" else "clear")
+
+
+def game():
+    clear()
+    print("You are driving out of town on a stormy night, it's getting late.\n"
+          "You're driving down country roads, the darkness is illuminated by\n"
+          "the headlights on your car and cracks of lightning.\n"
+          "You find yourself getting tired, you need to stop soon to rest.\n"
+          "After several more miles, you see a road leading off to a hotel\n"
+          "seeing nowhere else to stay, you decide to rest here for tonight.")
+    time.sleep(2)
+    print("Seeing other cars parked up, you'd guess around 10 other people\n"
+          "are staying here tonight as well, to wait out the storm.\n"
+          "Check in is quickly sorted, you don't bring in many bags\n"
+          "as you don't plan to stay here for long.\n"
+          "Finding your room on the second floor, sleep comes quickly\n"
+          "as you drift off to sound of heavy rain hitting the window") 
